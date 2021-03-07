e6eb0c17512df9313f9cfffa220d22a91042c407
NB <47492535+altmshfkgudtjr@users.noreply.github.com>
2021-03-07 21:22:45 +0900

    로깅 기능 추가

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const { autoUpdater } = require("electron-updater");
 const isDev = require('electron-is-dev');
+const log = require('electron-log')
 const path = require('path');
 const url = require('url');
 
@@ -20,44 +21,43 @@ function createWindow() {
 	}
 
 	/** 시작 URL 또는 파일 */
-	const startUrl = process.env.ELECTRON_START_URL || url.format({
+	const startUrl = url.format({
 		pathname: path.join(__dirname, '/../build/index.html'),
 		protocol: 'file:',
 		slashes: true
 	});
+	// const startUrl = process.env.ELECTRON_START_URL || url.format({
+	// 	pathname: path.join(__dirname, '/../build/index.html'),
+	// 	protocol: 'file:',
+	// 	slashes: true
+	// });
 	
 	/** 시작 포인트 실행 */
 	win.loadURL(startUrl);
 }
 
-/** 메세지 전송 함수 */
-function sendStatusToWindow(text) {
-	console.log(text);
-  // win.webContents.send('message', text);
-}
-
 /* Updater ======================================================*/
 
 autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('업데이트 확인 중...');
+	log.info('업데이트 확인 중...');
 });
 autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('업데이트가 가능합니다.');
+	log.info('업데이트가 가능합니다.');
 });
 autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('현재 최신버전입니다.');
+	log.info('현재 최신버전입니다.');
 });
 autoUpdater.on('error', (err) => {
-  sendStatusToWindow('에러가 발생하였습니다. 에러내용 : ' + err);
+	log.info('에러가 발생하였습니다. 에러내용 : ' + err);
 });
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+	let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
+	log.info(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('업데이트가 완료되었습니다.');
+	log.info('업데이트가 완료되었습니다.');
 });
 
 /* Electron =====================================================*/
@@ -68,8 +68,8 @@ app.on('ready', () => {
 	createWindow();
 
 	// 자동 업데이트 등록
-	// autoUpdater.checkForUpdates();
-	autoUpdater.checkForUpdatesAndNotify();
+	autoUpdater.checkForUpdates();
+	// autoUpdater.checkForUpdatesAndNotify();
 });
 
 /** [생명주기] 모든 창이 닫히면 자동으로 앱 종료 */
